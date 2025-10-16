b821145074c3479412329304351f6c92fe93cf72
kb603 <kojoaddobaffour@gmail.com>
2025-10-16 12:20:16 +0000

    expo-101

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function LoginScreen() {
   return (
     <View
       style={{
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Index / Home Page</Text>
+      <Text>Hello this is the login page</Text>
     </View>
   );
 }
